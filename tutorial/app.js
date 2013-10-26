db464256c671904352a3214c54635992927031f3
Donald Derek <donaldderek.haddad@gmail.com>
2013-10-26 03:24:43 +0300

    added mongodb

--- tutorial/app.js
+++ tutorial/app.js
@@ -5,21 +5,49 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var stylus = require('stylus');
 
 var app = express();
 
-// all environments
+//Getting port from env variables or 3000
 app.set('port', process.env.PORT || 3000);
+
+//Setting up the views directory
 app.set('views', __dirname + '/views');
+
+//Setting up our HTML preprocessor
 app.set('view engine', 'jade');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
+
+//Used for Submitting Forms
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+
+//Used for routes 
 app.use(app.router);
+
+//Setting up our CSS preprocessor
+app.use(
+  stylus.middleware({
+    src:  __dirname + "/views", 
+    dest: __dirname + "/public",
+    debug: true,
+    force: true,
+    compile : function(str, path) {
+      console.log('compiling');
+      return stylus(str)
+        .set('filename', path)
+        .set('warn', true)
+        .set('compress', true);
+    }
+  })
+ );
+
+//Setting up the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -27,9 +55,11 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//Routes
+app.post('/submit',routes.submit)
 app.get('/', routes.index);
-app.get('/users', user.list);
 
+//Create the server instance using express app
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- tutorial/routes/index.js
+++ tutorial/routes/index.js
@@ -1,8 +1,31 @@
 
 /*
- * GET home page.
+ * Routes functions, can be considered as a controller
  */
+var MongoClient = require('mongodb').MongoClient;
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'JSRevolutionII' });
 };
+
+exports.submit = function(req, res){
+	console.log(req.body);
+	// Connect to the db
+	MongoClient.connect("mongodb://localhost:27017/jsr", function(err, db) {
+  		
+  		if(err) { return console.dir(err); }
+			
+		db.createCollection('messages', function(err, collection) {});
+
+		var messages = db.collection('messages');
+		var msg = { 'name':req.body.name,
+					'email': req.body.email, 
+					'message':req.body.message, 
+					'Date': Date.now() };
+
+		messages.insert(msg, function(err, results){
+			console.log(results)
+			res.render('thanks', { title: 'thanks' });
+		});
+	})
+};	
